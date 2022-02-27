e073526f728db145062a7840d65a2383f27221e8
Gerald Tagle <78480062+GcTags@users.noreply.github.com>
2022-02-27 20:50:11 +0800

    Update Hash Location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -48,7 +48,8 @@ const config: SocketIoConfig = { url: 'http://localhost:3001', options: { autoCo
     ReactiveFormsModule,
     MatCarouselModule.forRoot()
   ],
-  providers: [],
+  providers: [    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
